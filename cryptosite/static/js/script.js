d8cc99531a639c1a5a73dd7876d6754ddadc15e8
Gaby Costales <gabycostales@gmail.com>
2018-03-25 15:50:23 -0400

    Basic styling and scripts setup

---
+++ cryptosite/static/js/script.js
@@ -0,0 +1,38 @@
+// Get Past 7 Weekdays for Sentiment X Axis
+const weekdays = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+let weekdayLabels = [];
+for (var i = 6; i >= 0; i--) {
+  var d = new Date();
+  d.setDate(d.getDate() - i);
+  console.log(d.getDay());
+  weekdayLabels.push(weekdays[d.getDay()]);
+}
+// console.log(weekdayLabels);
+
+// Sentiment Line Chart
+var ctx = document.getElementById("lineChart");
+var lineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: weekdayLabels,
+    datasets: [{
+      label: 'Sentiment',
+      data: [4, 5, 3, 6, 2, 7, 8],
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      borderColor: 'rgba(75, 192, 192, 0.2)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    legend: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+
